3cfb0a35beed83e111304321a2deae8961a64e73
Takagi-Keihin <admin@keihinsteeljp.onmicrosoft.com>
2026-04-07 17:02:53 +0900

    Update index.js

--- api/submit/index.js
+++ api/submit/index.js
@@ -1,12 +1,20 @@
 module.exports = async function (context, req) {
     const data = req.body;
 
-    context.log("受信データ:", data);
+    const flowUrl = "https://defaultf1d9068e3b94400c90302c92ac8dbc.f6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/064833865b844905a35b6c67a423567e/triggers/manual/paths/invoke?api-version=1";
+
+    await fetch(flowUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
 
     context.res = {
         status: 200,
         body: {
-            message: "データ受信成功",
+            message: "保存完了",
             data: data
         }
     };
